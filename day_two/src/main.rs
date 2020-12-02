3e2112ff87342987bd8bdc93a097da90dc52a065
Carl Campbell <carl-campbell-III@yandex.com>
2020-12-02 15:24:43 -0500

    Day Two Solution that crashes my pc when you put the 0.7gb input in and build

--- day_two/src/main.rs
+++ day_two/src/main.rs
@@ -1,3 +1,33 @@
+use std::collections::HashMap;
+use std::io::{stdin, BufRead};
 fn main() {
-    println!("Hello, world!");
+    let (mut p1, mut p2) = (0,0);
+        include_str!("../input")
+        .lines()
+        .map(|x| x.split(&['-', ' ', ':'][..])
+            .filter(|x| !x.is_empty())
+            .collect::<Vec<_>>())
+        .map(|x| (x[0].parse::<usize>().unwrap(), x[1].parse::<usize>().unwrap(), x[2].chars().next().unwrap(), x[3]))
+        .for_each(|(b, a, key, pass)| {
+            let n = n_occurrences(key, pass);
+            if b <= n && n <= a {
+                p1 += 1;
+            }
+            let hash = pass.char_indices().collect::<HashMap<_,_>>();
+            if !(hash.get(&(a - 1)).unwrap() == &key) != !(hash.get(&(b - 1)).unwrap() == &key) {
+                p2 += 1;
+            }
+        });
+    println!("Part one: {}", p1);
+    println!("Part two: {}", p2);
 }
+
+fn n_occurrences(key: char, pass: &str) -> usize {
+    let mut ctr: usize = 0;
+    for ch in pass.chars() {
+        if ch == key {
+            ctr += 1;
+        }
+    }
+    return ctr
+} 
