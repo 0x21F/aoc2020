681ab84dbd9cb263ce27c02c299be38295d21b61
Carl Campbell <carl-campbell-III@yandex.com>
2020-12-02 19:13:10 -0500

    Refactor and parallelizing the program

--- day_two/src/main.rs
+++ day_two/src/main.rs
@@ -1,33 +1,30 @@
-use std::collections::HashMap;
-use std::io::{stdin, BufRead};
-fn main() {
-    let (mut p1, mut p2) = (0,0);
-        include_str!("../input")
-        .lines()
-        .map(|x| x.split(&['-', ' ', ':'][..])
-            .filter(|x| !x.is_empty())
-            .collect::<Vec<_>>())
-        .map(|x| (x[0].parse::<usize>().unwrap(), x[1].parse::<usize>().unwrap(), x[2].chars().next().unwrap(), x[3]))
-        .for_each(|(b, a, key, pass)| {
-            let n = n_occurrences(key, pass);
-            if b <= n && n <= a {
-                p1 += 1;
-            }
-            let hash = pass.char_indices().collect::<HashMap<_,_>>();
-            if !(hash.get(&(a - 1)).unwrap() == &key) != !(hash.get(&(b - 1)).unwrap() == &key) {
-                p2 += 1;
-            }
-        });
-    println!("Part one: {}", p1);
-    println!("Part two: {}", p2);
-}
+use std::io::{stdin, Read};
+use rayon::str::ParallelString;
+use rayon::iter::ParallelIterator;
 
-fn n_occurrences(key: char, pass: &str) -> usize {
-    let mut ctr: usize = 0;
-    for ch in pass.chars() {
-        if ch == key {
-            ctr += 1;
-        }
-    }
-    return ctr
-} 
+
+fn main() {
+    let mut input = String::new();
+    stdin().lock().read_to_string(&mut input).unwrap();
+
+    let (part_one, part_two) = input.as_parallel_string()
+        .par_lines()
+        .map(|y| {
+            let x = y.split(&['-', ' ', ':'][..]).filter(|s| !s.is_empty()).collect::<Vec<_>>();
+
+            let b = x[0].parse::<usize>().unwrap();
+            let a = x[1].parse::<usize>().unwrap();
+            let key = x[2].chars().next().unwrap();
+            let pass = x[3];
+            let n = pass.matches(key).count();
+
+            ((b <= n && n <= a) as usize,
+            ((pass.chars().nth(a - 1).unwrap() == key) ^ (pass.chars().nth(b - 1).unwrap() == key)) as usize)
+        })
+        .fold(|| (0, 0), |z, (x, y)| {
+            (z.0 + x,
+            z.1 + y)
+        })
+        .reduce(||(0, 0), |a, (x,y)| (a.0 + x, a.1 + y));
+    println!("part one: {} \npart two: {}", part_one, part_two);
+}
